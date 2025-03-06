773c59e0b914ff074bbdbb218ae9361ce7fb2dd3
ishakson <ishaksylmz7@gmail.com>
2025-03-07 00:43:25 +0300

    main

---
+++ src/index.js
@@ -0,0 +1,68 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar image="/ishak.jpg" />
+      <div className="data">
+        <Intro 
+          name="İshak Söylemez" 
+          description="Frontend Developer | Passionate about UI/UX & Web Performance" 
+        />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar({ image }) {
+  return <img src={image} alt="Avatar" className="avatar" />;
+}
+
+function Intro({ name, description }) {
+  return (
+    <div className="intro">
+      <h1>{name}</h1>
+      <p className="description">{description}</p>
+    </div>
+  );
+}
+
+function SkillList() {
+  const skills = [
+    { skill: "React", color: "#61DBFB", emoji: "⚛️" },
+    { skill: "Vue", color: "#42b883", emoji: "🍃" },
+    { skill: "Angular", color: "#DD0031", emoji: "🅰️" },
+    { skill: "Svelte", color: "#FF3E00", emoji: "🪀" }
+  ];
+
+  return (
+    <div>
+      <h2>Skills</h2>
+      <ul className="skill-list">
+        {skills.map((s, index) => (
+          <Skill key={index} skill={s.skill} color={s.color} emoji={s.emoji} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Skill({ skill, color, emoji }) {
+  return (
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{emoji}</span>
+      <span>{skill}</span>
+    </div>
+  );
+}
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
